1e2ac4defd28f667eb5c77a42973269a1a738bee
Clinton Dmello <clinton.dmello95@gmail.com>
2021-06-03 23:20:07 +0530

    Completed single message Broadcast to all Clients

--- ChatRooms/ChatRoomsUI/Startup.cs
+++ ChatRooms/ChatRoomsUI/Startup.cs
@@ -1,14 +1,27 @@
+using ChatRoomsUI.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 
 namespace ChatRoomsUI
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services) { }
+
+        public IConfiguration Configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddSignalR();
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -18,14 +31,13 @@ namespace ChatRoomsUI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(name:"default",pattern:"{controller=ChatBase}/{action=Home}");
+                endpoints.MapRazorPages();
+                endpoints.MapHub<ChatHub>("/chatRooms");
             });
         }
     }
