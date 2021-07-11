898ab8c9c120bf216d095cd805bc59afc194cc61
Clinton Dmello <clinton.dmello95@gmail.com>
2021-07-11 22:14:47 +0530

    Added group functionality first phase

--- ChatRooms/ChatRoomsUI/wwwroot/js/ChatBase.js
+++ ChatRooms/ChatRoomsUI/wwwroot/js/ChatBase.js
@@ -5,29 +5,31 @@ document.getElementById("sendButton").disabled = true;
 
 connection.on("RecieveMessage", function (user, message) {
     var li = document.createElement("li");
+    li.style = "color:white";
     document.getElementById("messagesList").appendChild(li);
     li.textContent = `${user} says ${message}`;
 });
 
 connection.on("UserJoined", function (userDetails) {
     var li = document.createElement("li");
+    li.style = "color:white";
     document.getElementById("messagesList").appendChild(li);
     li.textContent = `${userDetails.userName} joined the chat`;
 });
 
 connection.on("UserDisconnected", function (user) {
     var li = document.createElement("li");
+    li.style = "color:white";
     document.getElementById("messagesList").appendChild(li);
     li.textContent = `${user} left the chat`;
 });
 
 
 function OnSuccessValidation() {
-    debugger;
     connection.start().then(function () {
         document.getElementById("sendButton").disabled = false;
-        connection.invoke("NewUser", { ConnectionId: connection.connectionId, UserName: document.getElementById("userInput").value }).catch(function (err) {
-            return console.err(err.toString());
+        connection.invoke("NewUser", { ConnectionId: connection.connectionId, UserName: document.getElementById("userInput").value, GroupName: document.getElementById("groupNameInput").value }).catch(function (err) {
+            return console.error(err.toString());
         })
     }).catch(function (err) {
         return console.error(err.toString());
